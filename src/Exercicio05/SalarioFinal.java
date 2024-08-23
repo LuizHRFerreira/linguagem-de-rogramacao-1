bcf04036f74fd6a285ca0d9d964d8acdcaa14df7
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2024-08-23 09:30:12 -0300

    Inclusion of scan method

--- src/Exercicio05/SalarioFinal.java
+++ src/Exercicio05/SalarioFinal.java
@@ -15,19 +15,22 @@ Salário fixo + comissão (Quantidade de carro vendido * Valor por carro vendido
 package Exercicio05;
 
 //Criei a classe "Salario Final"
+
+import java.util.Scanner;
+
 public class SalarioFinal {
 
 	// Aqui dei os atributos a classe, vai coletar todos as informações para efetuar
 	// o cálculo
 
-	private float QuantCarroVendido;
-	private float ValVendas;
-	private float Salario;
-	private float ComPorCarro;
+	private double Salario;
+	private int QuantCarroVendido;
+	private double ValVendas;
+	private double ComPorCarro;
 
 	// Criação do contrutor que vai vincular todas os valores do objeto para seus
 	// respectivos atributos
-	public SalarioFinal(float Salario, float QuantCarroVendido, float ValVendas, float ComPorCarro) {
+	public SalarioFinal(double Salario, int QuantCarroVendido, double ValVendas, double ComPorCarro) {
 		this.Salario = Salario;
 		this.QuantCarroVendido = QuantCarroVendido;
 		this.ValVendas = ValVendas;
@@ -42,12 +45,32 @@ public class SalarioFinal {
 	// Aqui criei o main que vai criar um objeto e exibindo ele na tela
 	public static void main(String[] Args) {
 
+		Scanner read = new Scanner(System.in);
+		System.out.println("============================== Cálculo de Salário do vendedor de carros ==============================");  
+		System.out.println("Este programa irá cálcular o valor total do salário que o vendedor de carros receberá.");  
+		System.out.println("");
+
+		System.out.print("Informe o valor do salário: R$");
+		double salario = read.nextDouble();
+
+		System.out.print("Informe a quantidade de carros vendidos: ");
+		int QuantCarroVendido = read.nextInt();
+
+		System.out.print("Informe o valor total das vendas: R$");
+		double ValVendas = read.nextDouble();
+
+		System.out.print("Informe a comissão fixa por carro vendido: R$");
+		double ComPorCarro = read.nextDouble();
+
 		// Criei objeto que chamei de Valor da classe ValCar e acionei o contrutor para
 		// colocar o valor de 100 no atributo factoryCost
-		SalarioFinal MontanteTotal = new SalarioFinal(1500, 3, 24000, 940);
+		SalarioFinal MontanteTotal = new SalarioFinal(salario, QuantCarroVendido, ValVendas, ComPorCarro);
 
 		// Exibe o resultado do código na tela
-		System.out.println("O valor total do salário do colaborador será de R$" + MontanteTotal.Montante());
+		System.out.println("");
+		System.out.println("O valor total do salário do colaborador será de R$" + String.format("%.2f",MontanteTotal.Montante()));
+	
+		read.close();
 	}
 
 }
--- src/Exercicio06/Converter.java
+++ src/Exercicio06/Converter.java
@@ -7,15 +7,17 @@ F = ( C × 1.8) + 32 */
 
 package Exercicio06;
 
-	// Criei a classe chamada Converter
+import java.util.Scanner;
+
+// Criei a classe chamada Converter
 public class Converter {
 
 	// Criei o atributo celsios dentro da classe Converter
-	private int fahrenheit;
+	private double fahrenheit;
 
 	// Criei um contrutor chamado Converter que vai adicionar o valor que eu
 	// destinar ao objeto à variavel fahrenheit
-	public Converter(int fahrenheit) {
+	public Converter(double fahrenheit) {
 		this.fahrenheit = fahrenheit;
 	}
 
@@ -26,9 +28,18 @@ public class Converter {
 
 	//Aqui criei o main que vai criar um objeto e exibindo ele na tela
 	public static void main(String[] Args) {
+
+		Scanner read = new Scanner(System.in);
+		
+		System.out.print("Informe a temperatura em fahrenheit: ");
+		double fahrenheit = read.nextDouble();
+
 		//Criei objeto que chamei de resultado da classe Converter e acionei o contrutor para colocar o valor de 100 no atributo fahrenheit
-		Converter resultado = new Converter(167);
+		Converter resultado = new Converter(fahrenheit);
+		
 		//Exibe o resultado da conversao
-		System.out.println("O valor de " + resultado.fahrenheit + "ºF é igual a " + resultado.Celsius() + "ºC");
+		System.out.println("O valor de " + String.format("%.2f",resultado.fahrenheit) + "ºF é igual a " + String.format("%.2f",resultado.Celsius()) + "ºC");
+
+		read.close();
 	}
 }
