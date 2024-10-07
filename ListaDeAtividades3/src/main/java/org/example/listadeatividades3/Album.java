f3d10646a2075d648a631aa13a584735dfdc87d3
Luiz Henrique Rabello Ferreira <91166115+LuizHRFerreira@users.noreply.github.com>
2024-10-06 22:43:15 -0300

    Upload da tela Album

--- ListaDeAtividades3/src/main/java/org/example/listadeatividades3/Album.java
+++ ListaDeAtividades3/src/main/java/org/example/listadeatividades3/Album.java
@@ -13,86 +13,33 @@ public class Album {
         this.nome = nome;
         this.dataDelancamento = dataDelancamento;
         this.Interprete = interprete;
-        System.out.println("teste");
     }
 
-    public String ouvir() {
+    public static String ouvir() {
+
         return "Começou a tocar o álbum";
     }
 
-    public String pausar() {
+    public static String pausar() {
+
         return "Pausou a música";
     }
 
-    public String baixar() {
+    public static String baixar() {
+
         return "O álbum está sendo baixado";
     }
 
-    public static void executar() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("=========== Album de música ===========");
-        System.out.println("Primeiramente, descreva o álbum");
-        System.out.print("Informe o nome do álbum: ");
-        String nome = scanner.nextLine();
-        System.out.print("Informe a data de lançamento: ");
-        String dataDelancamento = scanner.nextLine();
-        System.out.print("Informe o intérprete: ");
-        String Interprete = scanner.nextLine();
-
-        Album album = new Album(nome, dataDelancamento, Interprete);
-
-        int subOption;
-        do {
-            System.out.println("");
-            System.out.println("O que deseja fazer com o álbum " + album.nome + "?");
-            System.out.println("[01] Ouvir");
-            System.out.println("[02] Pausar");
-            System.out.println("[03] Baixar");
-            System.out.println("[00] Sair");
-            System.out.println("");
-            System.out.print("Resposta: ");
-
-            // Verifica se a entrada é um número inteiro válido
-            while (!scanner.hasNextInt()) {
-                System.out.println("Entrada inválida. Por favor, insira um número.");
-                scanner.next(); // Limpa a entrada inválida
-                System.out.print("Resposta: ");
-            }
-
-            subOption = scanner.nextInt();
-            scanner.nextLine(); // Limpa o buffer após nextInt()
-
-            switch (subOption) {
-                case 1:
-                    System.out.println(album.ouvir());
-                    break;
-
-                case 2:
-                    System.out.println(album.pausar());
-                    break;
-
-                case 3:
-                    System.out.println(album.baixar());
-                    break;
-
-                case 0:
-                    // Saída do loop
-                    System.out.println("Saindo...");
-                    break;
-
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
-                    break;
-            }
-
-            if (subOption != 0) {
-                System.out.println("Pressione Enter para continuar...");
-                scanner.nextLine(); // Aguarda a entrada do usuário para continuar
-            }
-
-        } while (subOption != 0);
-
-        System.out.println("Voltando ao menu principal...");
+    public String getTitulo() {
+    return nome;
     }
+
+    public String getInterprete() {
+    return Interprete;
+    }
+
+    public String getDataLancamento() {
+        return dataDelancamento;
+    }
+
 }
--- ListaDeAtividades3/src/main/java/org/example/listadeatividades3/AlbumApplication.java
+++ ListaDeAtividades3/src/main/java/org/example/listadeatividades3/AlbumApplication.java
@@ -2,19 +2,32 @@ package org.example.listadeatividades3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class AlbumApplication extends Application {
+
+    static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource("Album-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 502, 442);
+        //FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource("Album-view.fxml"));
+        scene = new Scene(loadFXML("Album-view"), 573, 482);
         stage.setTitle("Album");
         stage.setScene(scene);
         stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException{
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AlbumApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
